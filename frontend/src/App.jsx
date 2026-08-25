c055192aba8f6b4da2a7054c9f034d6a40d5729f
Naksh <nakshar2025@gmail.com>
2026-08-25 18:09:36 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     setResult(null)
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/analyze', {
+      const response = await fetch('https://surakshatext-backend.onrender.com/analyze', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
