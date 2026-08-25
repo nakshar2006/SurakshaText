64261e84301ebec248ac480576013d2dc415affe
Naksh <nakshar2025@gmail.com>
2026-08-25 18:20:47 +0530

    Allow Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -18,10 +18,16 @@ app.add_middleware(
    allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://suraksha-text.vercel.app"
+
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://suraksha-text.vercel.app"
+
     "http://localhost:5175",
-    "http://127.0.0.1:5175"
+    "http://127.0.0.1:5175",
+    "https://suraksha-text.vercel.app"
+
 ],
     allow_credentials=True,
     allow_methods=["*"],
